32f43871d1f911d2cabebec76406d52477631b85
Gobind Singh Maan <sgobind577@gmail.com>
2023-05-26 21:42:51 +0530

    new file:   lib/app_widgets/select.dart

---
+++ lib/app_widgets/select.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:notes_application/auth.dart';
+import 'package:notes_application/screens/home_screen.dart';
+import 'package:notes_application/screens/login_screen.dart';
+
+class Selector extends StatefulWidget {
+  const Selector({super.key});
+
+  @override
+  State<Selector> createState() => _SelectorState();
+}
+
+class _SelectorState extends State<Selector> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomeScreen();
+        } else {
+          return LoginScreen();
+        }
+      },
+    );
+  }
+}
