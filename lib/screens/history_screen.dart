4a3c6e60c6ebf8703f7b60160c16dbe7b2849947
Gobind Singh Maan <sgobind577@gmail.com>
2023-06-17 15:18:57 +0530

    modified:   lib/utils/auth.dart

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -8,29 +8,41 @@ import 'package:notes_application/global/dimensions.dart';
 import 'package:notes_application/global/global.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class HistoryScreen extends StatefulWidget {
-  const HistoryScreen({super.key});
+List<dynamic> orderIDs = [];
+final double height = Dimensions.screenHeight;
+final double width = Dimensions.screenWidth;
+
+class ReturnWidget extends StatefulWidget {
+  final String orderID;
+
+  const ReturnWidget({required this.orderID, super.key});
 
   @override
-  State<HistoryScreen> createState() => _HistoryScreenState();
+  State<ReturnWidget> createState() => _ReturnWidgetState();
 }
 
-class _HistoryScreenState extends State<HistoryScreen> {
-  final double height = Dimensions.screenHeight;
-  final double width = Dimensions.screenWidth;
-  List<dynamic> orderIDs = [];
+class _ReturnWidgetState extends State<ReturnWidget> {
+  Map<String, dynamic> orderInfo = {};
+
+  Future fetchOrder() async {
+    final orderDocRef = db.collection('orders').doc(widget.orderID);
+    final doc = await orderDocRef.get();
+    final data = doc.data() as Map<String, dynamic>;
+    orderInfo = data;
+  }
 
   void returnProduct({required String orderID}) async {
     DateTime endTimeStamp = DateTime.now();
     final orderDocRef = db.collection('orders').doc(orderID);
-    final doc = await orderDocRef.get();
-    Map<String, dynamic> orderInfo = doc.data() as Map<String, dynamic>;
     final productDocRef = db.collection('products').doc(orderInfo['productID']);
     final prodDoc = await productDocRef.get();
     final prodInfo = prodDoc.data();
     Timestamp startTime = orderInfo['startTimeStamp'];
     DateTime startTimeStamp = startTime.toDate();
-    double pricePaid = (endTimeStamp.difference(startTimeStamp).inMinutes * prodInfo!['pricePerHour'] / 60).toPrecision(2);
+    double pricePaid = (endTimeStamp.difference(startTimeStamp).inMinutes *
+            prodInfo!['pricePerHour'] /
+            60)
+        .toPrecision(2);
     bool paymentConfirmation = false;
     await showDialog(
       barrierDismissible: false,
@@ -52,25 +64,125 @@ class _HistoryScreenState extends State<HistoryScreen> {
               child: const Text("Cancel"),
             )
           ],
-          content: Row(
-            children: [
-              const CircularProgressIndicator(),
-              Container(
-                  margin: const EdgeInsets.only(left: 7),
-                  child: Text("Price to be paid is: \u{20B9}${pricePaid.toPrecision(2)}")),
-            ],
+          content: Container(
+            margin: const EdgeInsets.only(left: 7),
+            child: Text(
+              "Price to be paid is: \u{20B9}${pricePaid.toPrecision(2)}",
+            ),
           ),
         );
       },
     );
-    if(paymentConfirmation) {
+    if (paymentConfirmation) {
       orderDocRef.update({
-        'endTimeStamp' : endTimeStamp,
-        'pricePaid':pricePaid.toPrecision(2),
+        'endTimeStamp': endTimeStamp,
+        'pricePaid': pricePaid.toPrecision(2),
       });
       Fluttertoast.showToast(msg: 'Order returned successfully');
+      setState(() {});
     }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: fetchOrder(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              width: width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(height / 43.85),
+                ),
+                color: Colors.blue.shade400,
+              ),
+              child: const Text(
+                'Loading...',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              width: width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(height / 43.85),
+                ),
+                color: Colors.blue.shade400,
+              ),
+              child: const Text(
+                'Error occured!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          );
+        } else {
+          if (orderInfo['pricePaid'] == null) {
+            return Expanded(
+              child: InkWell(
+                onTap: () {
+                  returnProduct(orderID: widget.orderID);
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(height / 43.85),
+                    ),
+                    color: Colors.blue.shade400,
+                  ),
+                  child: const Text(
+                    'Return',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                width: width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(height / 43.85),
+                  ),
+                  color: Colors.blue.shade400,
+                ),
+                child: Text(
+                  "Price paid : ${orderInfo['pricePaid']}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            );
+          }
+        }
+      },
+    );
+  }
+}
+
+class HistoryScreen extends StatelessWidget {
+  const HistoryScreen({super.key});
 
   Future fetchHistory() async {
     try {
@@ -79,7 +191,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
       final userData = doc.data() as Map<String, dynamic>;
       orderIDs = userData['history'];
     } catch (e) {
-      print(e);
+      //print(e);
     }
   }
 
@@ -93,70 +205,6 @@ class _HistoryScreenState extends State<HistoryScreen> {
     return prodData;
   }
 
-  Future<Widget> createReturn({required String orderID}) async {
-    final orderDocRef = db.collection('orders').doc(orderID);
-    final doc = await orderDocRef.get();
-    final docData = doc.data() as Map<String, dynamic>;
-    if (docData['endTimeStamp'] == null) {
-      print('return');
-      return Expanded(
-        child: InkWell(
-          onTap: () {
-            returnProduct(orderID: orderID);
-            setState(() {});
-          },
-          child: Container(
-            width: width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(height / 43.85),
-              ),
-              color: Colors.cyan,
-            ),
-            child: const Text('Return'),
-          ),
-        ),
-      );
-    } else {
-      print('no return');
-      return Expanded(
-        child: Container(
-          width: width,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(height / 43.85),
-            ),
-            color: Colors.cyan,
-          ),
-          child: Text("Price paid : ${docData['pricePaid']}"),
-        ),
-      );
-    }
-  }
-
-  Widget createLast(String orderID) {
-    return FutureBuilder<Widget>(
-      future: createReturn(orderID: orderID),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return const Text("Error occurred! Try again later...");
-        } else if (snapshot.hasData) {
-          return snapshot.data!;
-        } else {
-          return Container();
-        }
-      },
-    );
-  }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   fetchHistory();
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -169,7 +217,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
           ),
           onPressed: () => Navigator.of(context).pop(),
         ),
-        backgroundColor: Colors.cyan,
+        backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
         title: const HeadingText(
           'History',
@@ -324,12 +372,14 @@ class _HistoryScreenState extends State<HistoryScreen> {
                                         Text(
                                           "\u{20B9}${prodData['pricePerHour']} / hr",
                                           style: const TextStyle(
-                                            fontSize: 18,
+                                            fontSize: 17,
                                             color: Colors.black,
                                             fontWeight: FontWeight.bold,
                                           ),
                                         ),
-                                        createLast(orderIDs[index]),
+                                        ReturnWidget(
+                                          orderID: orderIDs[index],
+                                        ),
                                       ],
                                     ),
                                   ),
